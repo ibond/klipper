b00e66615b5459e9f88c73f0dc17d608dcdd1b4b
Ian Bond <ian.d.bond@gmail.com>
2019-09-27 18:11:16 -0400

    Adding new mesh.

--- klippy/extras/extruder_mesh.py
+++ klippy/extras/extruder_mesh.py
@@ -21,13 +21,22 @@ class ExtruderMesh:
         self.points_x = 5
         self.points_y = 5
 
+        # self.points = [
+        #     0.241, 0.277, 0.282, 0.283, 0.262,
+        #     0.183, 0.210, 0.221, 0.220, 0.201,
+        #     0.114, 0.133, 0.141, 0.140, 0.126,
+        #     0.054, 0.048, 0.046, 0.047, 0.049,
+        #     0.027, 0.012, 0.000, 0.003, 0.018
+        # ]
         self.points = [
-            0.241, 0.277, 0.282, 0.283, 0.262,
-            0.183, 0.210, 0.221, 0.220, 0.201,
-            0.114, 0.133, 0.141, 0.140, 0.126,
-            0.054, 0.048, 0.046, 0.047, 0.049,
-            0.027, 0.012, 0.000, 0.003, 0.018
+            0.401805, 0.462200, 0.470535, 0.471648, 0.436907,
+            0.305142, 0.350132, 0.368730, 0.367385, 0.334862,
+            0.190713, 0.221540, 0.235570, 0.233145, 0.209967,
+            0.089688, 0.079233, 0.076223, 0.078053, 0.081463,
+            0.044498, 0.019547, 0.000000, 0.005705, 0.029800
         ]
+
+        self.multiplier = 1.0
 
         self.gcode = self.printer.lookup_object('gcode')
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
@@ -50,7 +59,7 @@ class ExtruderMesh:
             self.next_transform.move([x, y, z, e + offset], speed)
         else:
             self.next_transform.move(newpos, speed)
-    
+
     def get_position(self):
         return self.next_transform.get_position()
 
@@ -71,11 +80,11 @@ class ExtruderMesh:
         iy0 = int(math.floor(lookup_y))
         iy1 = int(math.floor(lookup_y + 1))
 
-        #x0y0 = self.points[iy0 * self.points_x + ix0]
-        #x0y1 = self.points[iy1 * self.points_x + ix0]
-        #x1y0 = self.points[iy0 * self.points_x + ix1]
-        #x1y1 = self.points[iy1 * self.points_x + ix1]
-        
+        # x0y0 = self.points[iy0 * self.points_x + ix0]
+        # x0y1 = self.points[iy1 * self.points_x + ix0]
+        # x1y0 = self.points[iy0 * self.points_x + ix1]
+        # x1y1 = self.points[iy1 * self.points_x + ix1]
+
         # test points are transposed
         x0y0 = self.points[ix0 * self.points_x + iy0]
         x0y1 = self.points[ix0 * self.points_x + iy1]
@@ -88,12 +97,14 @@ class ExtruderMesh:
         xa = ((1-px) * x0y0) + (px * x1y0)
         xb = ((1-px) * x0y1) + (px * x1y1)
 
-        result = ((1-py) * xa) + (py * xb)
+        result = (((1-py) * xa) + (py * xb)) * self.multiplier
 
-        logging.info("extruder_mesh: (%.2f,%.2f,%.4f) -> %f" % (x, y, e, result))
+        logging.info("extruder_mesh: (%.2f,%.2f,%.4f) -> %f" %
+                     (x, y, e, result))
         logging.info("extruder_mesh: (%.2f,%.2f)" % (lookup_x, lookup_y))
         logging.info("extruder_mesh: (%d,%d,%d,%d)" % (ix0, ix1, iy0, iy1))
-        logging.info("extruder_mesh: (%.3f,%.3f,%.3f,%.3f)" % (x0y0, x1y0, x0y1, x1y1))
+        logging.info("extruder_mesh: (%.3f,%.3f,%.3f,%.3f)" %
+                     (x0y0, x1y0, x0y1, x1y1))
 
         return result
 
